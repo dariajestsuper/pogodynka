b326bcf312b8e93cedffaceacb9467b8f23b1f55
dariajestsuper <daria.rudowicz@gmail.com>
2022-10-29 09:42:30 +0200

    after lab04

---
+++ src/Form/MeasurementType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Measurement;
+use App\Entity\Location;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class MeasurementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date')
+            ->add('celsius')
+            ->add('location', EntityType::class, [
+                'class' => Location::class,
+                'choice_label' => 'city',
+                'choice_value' => 'id'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Measurement::class,
+        ]);
+    }
+}
